ee4db42489a940c1d8cecc34ba129327ec2a7f87
Jack <johnpmesserly@gmail.com>
2023-12-28 22:48:37 -0500

    added some more example images

--- algorithm/load_url.py
+++ algorithm/load_url.py
@@ -37,4 +37,4 @@ if __name__ == "__main__":
   sabers = "https://images.unsplash.com/photo-1603757981609-9aa146d91fb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
   garage = "https://images.unsplash.com/photo-1665973605924-3db7ed60ffa5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
   skating = "https://images.pexels.com/photos/5788775/pexels-photo-5788775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-  save_image_from_url(vsco_girl)
+  save_image_from_url(garage)
