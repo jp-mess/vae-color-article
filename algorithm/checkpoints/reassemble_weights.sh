573e77d9b7c06281ee75d6bcf030df9c01711985
Jack <johnpmesserly@gmail.com>
2023-12-29 21:36:34 -0500

    removed algorithm folder

--- algorithm/checkpoints/reassemble_weights.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Reassemble the latent_balancer.pth file from the parts
-cat latent_balancer_part_* > latent_balancer.pth
-
-echo "Weights have been reassembled into latent_balancer.pth."
-
--- algorithm/checkpoints/split_weights.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Split the latent_balancer.pth file into two parts
-split -n 2 -d latent_balancer.pth latent_balancer_part_
-
-echo "Weights have been split into two parts."
-
